5ab12a754da692ac7ebc43068e643e5656a3010c
codernmx <2507128400@qq.com>
2022-04-28 21:25:46 +0800

    贝壳

---
+++ Djan-框架/MuTouSite/MuTouSite/urls.py
@@ -0,0 +1,26 @@
+"""MuTouSite URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from . import views  # 修改的部分
+
+urlpatterns = [
+    path(r'', views.index),
+    path('admin/', admin.site.urls),
+    path(r'fang/', views.fang),
+    path(r'lei/', views.lei),
+    path(r'big/', views.big),
+]
